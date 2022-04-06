5a25f1e26291d296d975a69846e8e8dc731c5f1a
marcymarko <82332440+marcymarko@users.noreply.github.com>
2022-04-05 21:41:30 -0400

    Revert "fixed slug issues"
    This reverts commit 884c56fa0d5006d7957e3be845ac7185e63ad893.

--- Startup.cs
+++ Startup.cs
@@ -97,19 +97,11 @@ namespace TheBlogProject
                 //    pattern: "BlogPosts/UrlFriendly/{slug}",
                 //    defaults: new { controller = "Posts", action = "Details" });
 
-                endpoints.MapControllerRoute(
-                    name: "SlugRoute",
-                    pattern: "BlogPosts/UrlFriendly/{slug}",
-                    defaults: new { controller = "Posts", action = "Details" }
-                    );
 
 
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
-
-
-
                 endpoints.MapRazorPages();
             });
         }
