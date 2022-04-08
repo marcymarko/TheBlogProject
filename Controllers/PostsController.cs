5a1f928f7984ea8958ae36b3cdcef32ef259aa47
marcymarko <82332440+marcymarko@users.noreply.github.com>
2022-04-08 13:19:49 -0400

    Revert "Revert "almost""
    This reverts commit 6d86c060abf10fdebf153f90f4b73a4750a99e66.

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -12,6 +12,7 @@ using TheBlogProject.Models;
 using TheBlogProject.Services;
 using TheBlogProject.Enums;
 using X.PagedList;
+using TheBlogProject.ViewModels;
 
 namespace TheBlogProject.Controllers
 {
@@ -82,18 +83,32 @@ namespace TheBlogProject.Controllers
             }
 
             var post = await _context.Posts
-                .Include(p => p.Blog)
+                //.Include(p => p.Blog)
                 .Include(p => p.BlogUser)
                 .Include(p => p.Tags)
                 .Include(p => p.Comments)
                 .ThenInclude(c => c.BlogUser)
+                .Include(p => p.Comments)
+                .ThenInclude(c => c.Moderator)
                 .FirstOrDefaultAsync(m => m.Slug == slug);
             if (post == null)
             {
                 return NotFound();
             }
 
-            return View(post);
+            // new var dataVM
+            var dataVM = new PostDetailViewModel()
+            {
+                Post = post,
+                Tags = _context.Tags
+                                .Select(t => t.Text.ToLower())
+                                .Distinct().ToList()
+            };
+
+            ViewData["HeaderImage"] = _imageService.DecodeImage(post.ImageData, post.ContentType);
+            ViewData["MainText"] = post.Title;
+            ViewData["SubText"] = post.Abstract;
+            return View(dataVM);
         }
 
         // GET: Posts/Create
